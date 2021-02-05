fb121f2b0e7a4256a56bd19a6ec0752f6527b0cc
Raeed Hassan <32547662+raeedh@users.noreply.github.com>
2021-02-04 19:14:40 -0500

    fixed magnitude spectrum

--- Lab 1/lab1.m
+++ Lab 1/lab1.m
@@ -1,62 +1,40 @@
-%% square wave generator
 clc
 clear all
-hold off
 
-f0= 10000;     %fundamental freq of input square wave, 0.1ms period
-T0 = 1/f0;  %period 
+%% Square wave generator (input)
+f0 = 10000; % fundamental freq of input square wave
+T0 = 1/f0;  % period
 tstep = 0.005*T0;
-no_sample = 3*T0/tstep + 1; %no. of samples  within  3*T0
-no_sample1 = int32(T0/tstep + 1); %no. of samples  within  T0
-%tt = -0.5*T0:tstep:0.5*T0;
-tt = -1.5*T0:tstep:1.5*T0;
+tt = -3*T0:tstep:3*T0;
+fs = 1/tstep;   % sampling frequency
 
-% tt1 = -0.5*T0:tstep:0.5*T0; % time vector for the period -0.5T0 to 0.5T0
-% gp1 = [zeros(1,floor(0.5*length(tt1))) ones(1,ceil(0.5*length(tt1)))]; %input - triangular wave in the period -0.5T0 to 0.5T0
-gp1 = square(2 * pi * f0 * tt, 50);
-% gp_in = [gp1 gp1(2:no_sample1-1) gp1]; %3 cycles of the triangular wave
+input = square(tt*2*pi*f0,50); % input square wave
+N = length(input);
+
+% plot square wave
 figure(1)
-Hp1 = plot(tt,gp1);
+Hp1 = plot(tt,input);
 set(Hp1,'LineWidth',2)
 Ha = gca;
 set(Ha,'Fontsize',16)
-title('input - time domain')
-pause
+xlim([tt(1) tt(length(tt))]);
+title('Input - Time Domain')
 
-%% Fourier series representation of signal (Amplitude Spectrum)
-      
-K=1/(2*pi);
-N=100; %no. of harmonics
-nvec = -N:N;
-c_in = zeros(size(nvec));
-for n = nvec
-    m = n+N+1;
-    c_in(m) = 1i*K*((-1)^n)/n;
-    
-    if (n == 0)
-      c_in(m) = 0.0;
-    end
-end
-f = nvec*f0; %frequency vector
+%% Frequency response of input
+inputFFT = abs(fft(input))/N;
+fshift = (-N/2:N/2-1)*(fs/N);
+
+% plot amplitude spectrum
 figure(2)
-Hp1=stem(f,abs(c_in));
-axis([-8*f0 8*f0 0 max(abs(c_in))])
+Hp1 = plot(fshift, fftshift(inputFFT));
 set(Hp1,'LineWidth',2)
 Ha = gca;
 set(Ha,'Fontsize',16)
-title('magnitude spectrum of input')
-pause
+xlim([-8*f0 8*f0]);
+xlabel('Frequency (Hz)');
+ylabel('Amplitude');
+title('Magnitude Spectrum of Input');
 
-%% Fourier series representation of signal (Phase Spectrum)
-
-figure(3)
-Hp1=stem(f,angle(c_in));
-set(Hp1,'LineWidth',2)
-Ha = gca;
-set(Ha,'Fontsize',16)
-axis([-0.1e4 0.1e4 -pi pi])
-title('phase spectrum of input')
-pause
 
 %% Designing the 2nd order Butterworth filter parameters
 n = 2; % order of butterworth filter
@@ -86,37 +64,37 @@ b = 1;
 %mag = 20*log10(abs(h)); %% convert magnitude to dB
 %phase = angle(h);
 %phasedeg = phase*180/pi;
-Hf = 1 ./ (1 + 1.414*(1i*f/fc) + (1i*f/fc).^2); 
+%Hf = 1 ./ (1 + 1.414*(1i*f/fc) + (1i*f/fc).^2); 
 
-c_out = c_in .* Hf ; %Fourier coefficients of the filter output
+%c_out = c_in .* Hf ; %Fourier coefficients of the filter output
 
-figure(4)
-stem(f,abs(c_in),'r','LineWidth',2);
-hold on
-stem(f,abs(c_out),'b','LineWidth',2);
-hold off
-axis([-8*f0 8*f0 0 max(abs(c_in))])
-Ha = gca;
-set(Ha,'Fontsize',16)
-title('magnitude spectrum of filter output and input')
-Ha = gca;
-set(Ha,'Fontsize',16)
-legend('input','output')
-pause
+% figure(4)
+% stem(f,abs(c_in),'r','LineWidth',2);
+% hold on
+% stem(f,abs(c_out),'b','LineWidth',2);
+% hold off
+% axis([-8*f0 8*f0 0 max(abs(c_in))])
+% Ha = gca;
+% set(Ha,'Fontsize',16)
+% title('magnitude spectrum of filter output and input')
+% Ha = gca;
+% set(Ha,'Fontsize',16)
+% legend('input','output')
+% pause
 
 %% Construct the output signal from the Cout Fourier coefficients
 
-A = zeros(2*N+1,ceil(no_sample));
-for n = nvec
-    m=n+N+1;
-    A(m,:) = c_out(m) .* exp(1i*2*pi*n*f0*tt);
-end
-gp_out = sum(A);
-figure(5)
-Hp1 = plot(tt,real(gp_out),'b',tt,gp1,'r');
-set(Hp1,'LineWidth',2)
-Ha = gca;
-set(Ha,'Fontsize',16)
-title('filter input and output-time domain')
-set(Ha,'Fontsize',16)
-legend('output','input')
+% A = zeros(2*N+1,ceil(no_sample));
+% for n = nvec
+%     m=n+N+1;
+%     A(m,:) = c_out(m) .* exp(1i*2*pi*n*f0*tt);
+% end
+% gp_out = sum(A);
+% figure(5)
+% Hp1 = plot(tt,real(gp_out),'b',tt,gp1,'r');
+% set(Hp1,'LineWidth',2)
+% Ha = gca;
+% set(Ha,'Fontsize',16)
+% title('filter input and output-time domain')
+% set(Ha,'Fontsize',16)
+% legend('output','input')
